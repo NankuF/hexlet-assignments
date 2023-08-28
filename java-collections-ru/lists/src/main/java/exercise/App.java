691378a1a40ea66936db2580f280f8e2f6d13636
@hexlet/cli <support@hexlet.io>
2023-08-28 21:10:32 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,38 +1,44 @@
 package exercise;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
 class App {
-    public static void main(String[] args){
-        scrabble("rkqodlw", "world");
+    public static void main(String[] args) {
+        scrabble("begsdhhtsexoult", "Hexlet");
     }
+
     public static boolean scrabble(String text, String word) {
         char[] chars = text.toCharArray();
         char[] words = word.toCharArray();
         List<Character> list = new ArrayList<>();
-        if ( text.length() == 0 || word.length() == 0 || text.length() < word.length()) {
+        List<Character> list_chars = new ArrayList<>();
+        List<Character> list_words = new ArrayList<>();
+        for (var c : chars) {
+            list_chars.add(Character.toLowerCase(c));
+        }
+        for (var w : words) {
+            list_words.add(Character.toLowerCase(w));
+        }
+
+        if (text.length() == 0 || word.length() == 0 || text.length() < word.length()) {
             return false;
         }
-        for (var w: words){
-            for (var c : chars) {
-                if (c == w) {
-                    list.add(w);
-                }
+        for (var el : list_words) {
+            if (list_chars.contains(el)) {
+                list.add(el);
+                list_chars.remove(el);
             }
         }
 
-        // var new_world = String.join(", ", list);
-        var new_world = list.stream().map(Object::toString).collect(Collectors.joining(""));
-        // System.out.println(new_world);
-        if (new_world.equals(word)) {
-            // System.out.println(new_world + " " + word);
+        var result = list.stream().map(Object::toString).collect(Collectors.joining("")).toLowerCase();
+        System.out.println(result + " " + word);
+        if (result.equalsIgnoreCase(word)) {
             return true;
         }
         return false;
     }
 }
-//END
+// END
