0000430d0c80c273afa1e5cc2f0efe3faff62a09
@hexlet/cli <support@hexlet.io>
2023-08-30 09:13:34 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -1,10 +1,7 @@
 package exercise;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 // BEGIN
